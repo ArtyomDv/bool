1cf4240af7b1389e1aeab9711334cc57e649b77a
ArtyomDv <artiom.dovlatyan@gmail.com>
2024-10-03 20:51:02 +0400

    Add files via upload

---
+++ lesson1.py
@@ -0,0 +1,63 @@
+text = "Hello World"
+a = 10
+b = 20
+print(a, b)
+c = a
+a = b
+b = c
+print(a, b)
+                                #3
+a = 30
+b = 40
+print(a, b)
+c = a
+d = b
+a = d
+b = c
+print(a, b)
+                                #4
+a, b = 12, 13
+print(a, b)
+a, b = b, a
+print(a, b)
+                                #5
+a = 9.818
+print(a)
+a = int(a)
+print(a)
+a = float(a)
+print(a)
+a = str(a)
+print(a)
+a = bool(a)
+print(a)
+                                #6
+b = 3.14
+c = 0
+d = "pi"
+e = ""
+print(b)
+print(c)
+print(d)
+print(e)
+b = bool(b)
+c = bool(c)
+d = bool(d)
+e = bool(e)
+print(b)
+print(c)
+print(d)
+print(e)
+                                #1
+#text = "Hello World"
+#print(text)
+#text[0]="10.2"
+#print(text)
+
+                                #7
+a = bool(a)
+b = bool(b)
+c = a + (a * b)
+print(c)
+d = a * (a + b)
+print(d)
